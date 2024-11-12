bf40f934676d0b3ec9ee8256764a00d428972526
Lakshya10x <lakshyasharma8946@gmail.com>
2024-11-12 14:43:20 +0530

    Resolve Minor

--- src/main/java/com/project/springboot/CurrencyConfiguration/SpringBootPlaygroundApplication.java
+++ src/main/java/com/project/springboot/CurrencyConfiguration/SpringBootPlaygroundApplication.java
@@ -1,4 +1,4 @@
-package com.project.springboot.CurrencyConfiguration
+package com.project.springboot.CurrencyConfiguration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
