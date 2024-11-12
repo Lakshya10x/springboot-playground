5b5560dbee5c7d56b8afc487765d2c983b3425f0
Lakshya10x <lakshyasharma8946@gmail.com>
2024-11-12 14:41:49 +0530

    Add Simple Course REST API

--- src/main/java/com/project/springboot/SimpleCourseRESTAPI/SimpleCourseRESTAPIApplication.java
+++ src/main/java/com/project/springboot/SimpleCourseRESTAPI/SimpleCourseRESTAPIApplication.java
@@ -1,11 +1,11 @@
-package com.project.springboot.CurrencyConfiguration
+package com.project.springboot.SimpleCourseRESTAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootPlaygroundApplication {
+public class SimpleCourseRESTAPIApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootPlaygroundApplication.class,args);
+        SpringApplication.run(SimpleCourseRESTAPIApplication.class,args);
     }
 }
